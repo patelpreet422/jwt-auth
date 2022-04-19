ebdcb593421a1330db3f917990ff52395ff254ae
Preet Patel <patelpreet422@gmail.com>
2022-04-19 23:13:27 +0530

    add Swagger 3 support

--- src/main/java/com/github/patelpreet422/JwtLoginApplication.java
+++ src/main/java/com/github/patelpreet422/JwtLoginApplication.java
@@ -1,9 +1,13 @@
 package com.github.patelpreet422;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "Basic", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class JwtLoginApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/patelpreet422/controller/HelloController.java
+++ src/main/java/com/github/patelpreet422/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.github.patelpreet422.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@SecurityRequirement(name = "Basic")
 public class HelloController {
   @GetMapping("/")
   String hello() {
